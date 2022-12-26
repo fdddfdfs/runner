0ba3c9caae7d77785e8d8c62d33f89a330ff6397
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2022-12-26 10:44:18 +0400

    Multiline money spawn

--- MoneySpawner.cs
+++ MoneySpawner.cs
@@ -9,19 +9,22 @@ public class MoneySpawner
     private readonly float _speed;
     private readonly float _playerHalfHeight;
     private readonly FactoryPool<Money> _moneyPool;
+    private readonly float[] _spawnLines;
 
     public MoneySpawner(
         MoneyFactory<Money> moneyFactory,
         float height,
         float gravity,
         float speed,
-        float playerHeight)
+        float playerHeight,
+        float[] spawnLines = null)
     {
         _height = height;
         _gravity = gravity;
         _speed = speed;
         _playerHalfHeight = playerHeight / 2;
         _moneyPool = new FactoryPool<Money>(moneyFactory, null, true);
+        _spawnLines = spawnLines;
     }
     
     public void SpawnMoneys(Vector3 startPosition, float activeTime)
@@ -34,6 +37,7 @@ public class MoneySpawner
         int counter = 0;
         int maxCounter = (int)(activeTime * _speed / MoneyDistance);
         float currentGravity = 0;
+
         while (counter < maxCounter)
         {
             currentGravity += gravity * timeStep;
@@ -49,11 +53,26 @@ public class MoneySpawner
             currentPosition += distance;
             if (currentPosition > MoneyDistance)
             {
-                position = new Vector3(
-                    Level.GetClosestColumn(position.x),
-                    currentGravity + _playerHalfHeight,
-                    position.z + MoneyDistance);
-                _moneyPool.GetItem().transform.position = position;
+                if (_spawnLines != null)
+                {
+                    for (int i = 0; i < _spawnLines.Length; i++)
+                    {
+                        position = new Vector3(
+                            _spawnLines[i],
+                            currentGravity + _playerHalfHeight,
+                            position.z + MoneyDistance);
+                        _moneyPool.GetItem().transform.position = position;
+                    }
+                }
+                else
+                {
+                    position = new Vector3(
+                        Level.GetClosestColumn(position.x),
+                        currentGravity + _playerHalfHeight,
+                        position.z + MoneyDistance);
+                    _moneyPool.GetItem().transform.position = position;
+                }
+
                 currentPosition = 0;
                 counter++;
             }
