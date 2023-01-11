826ffaef6cb532e9ab16247473345db94f7b130e
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-01-12 01:30:08 +0400

    Added roll timeout check

--- Scripts/Player/Input/PlayerRunInput.cs
+++ Scripts/Player/Input/PlayerRunInput.cs
@@ -93,7 +93,7 @@ public sealed class PlayerRunInput : IRunnable
             _isRightPressed = Keyboard.current.dKey.wasPressedThisFrame;
         }
 
-        if (!_isRollPressed)
+        if (!_isRollPressed && !_isRollTimeout)
         {
             _isRollPressed = Keyboard.current.sKey.wasPressedThisFrame;
         }
