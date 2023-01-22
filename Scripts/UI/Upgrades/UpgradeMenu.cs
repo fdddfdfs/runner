7abb6285b06ad7df564273332fa133e0d4d57b2e
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-01-23 02:17:03 +0400

    Added upgrade menu scaling

--- Scripts/UI/Upgrades/UpgradeMenu.cs
+++ Scripts/UI/Upgrades/UpgradeMenu.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public sealed class UpgradeMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _menu;
-    [SerializeField] private Transform _parent;
+    [SerializeField] private RectTransform _parent;
     [SerializeField] private GameObject _upgradeItemPrefab;
     [SerializeField] private List<ItemType> _itemTypes;
     [SerializeField] private List<Sprite> _itemSprites;
@@ -16,19 +17,39 @@ public sealed class UpgradeMenu : MonoBehaviour
     {
         _menu.SetActive(visible);
     }
-    
+
     private void Start()
     {
         _upgradeActions = new Dictionary<ItemType, (Func<int> getLevel, Action increaseLevel)>
         {
-            { ItemType.Fly, (()=> Stats.FlyLevel.Value, ()=> Stats.FlyLevel.Value += 1) },
-            { ItemType.Immune, (()=> Stats.ImmuneLevel.Value, ()=> Stats.ImmuneLevel.Value += 1) },
-            { ItemType.Magnet, (()=> Stats.MagnetLevel.Value, ()=> Stats.MagnetLevel.Value += 1) },
-            { ItemType.DoubleMoney, (()=> Stats.DoubleMoneyLevel.Value, ()=> Stats.DoubleMoneyLevel.Value += 1) },
-            { ItemType.HighJump, (()=> Stats.HighJumpLevel.Value, ()=> Stats.HighJumpLevel.Value += 1) },
-            { ItemType.DoubleScore, (()=> Stats.DoubleScoreLevel.Value, ()=> Stats.DoubleScoreLevel.Value += 1) },
-            { ItemType.Board, (()=> Stats.BoardLevel.Value, ()=> Stats.BoardLevel.Value += 1) },
+            { ItemType.Fly, (() => Stats.FlyLevel.Value, () => Stats.FlyLevel.Value += 1) },
+            { ItemType.Immune, (() => Stats.ImmuneLevel.Value, () => Stats.ImmuneLevel.Value += 1) },
+            { ItemType.Magnet, (() => Stats.MagnetLevel.Value, () => Stats.MagnetLevel.Value += 1) },
+            { ItemType.DoubleMoney, (() => Stats.DoubleMoneyLevel.Value, () => Stats.DoubleMoneyLevel.Value += 1) },
+            { ItemType.HighJump, (() => Stats.HighJumpLevel.Value, () => Stats.HighJumpLevel.Value += 1) },
+            { ItemType.DoubleScore, (() => Stats.DoubleScoreLevel.Value, () => Stats.DoubleScoreLevel.Value += 1) },
+            { ItemType.Board, (() => Stats.BoardLevel.Value, () => Stats.BoardLevel.Value += 1) },
         };
+
+        Dictionary<ItemType, (string name, string description)> itemInfo = new()
+        {
+            [ItemType.Fly] = ("Plane", "Allow you to fly"),
+            [ItemType.Immune] = ("Immune", "Makes you invulnerable"),
+            [ItemType.Magnet] = ("Magnet", "Magnet for coins"),
+            [ItemType.DoubleMoney] = ("DoubleMoney", "Increase your money gain by 2 times"),
+            [ItemType.HighJump] = ("HighJump", "Makes you jump higher"),
+            [ItemType.DoubleScore] = ("DoubleScore", "Increase your score gain by 2 times"),
+            [ItemType.Board] = ("Board", "Allow you to take one hit"),
+        };
+
+        var parentVerticalLayoutGroup = _parent.GetComponent<VerticalLayoutGroup>();
+        _parent.SetSizeWithCurrentAnchors(
+            RectTransform.Axis.Vertical,
+            (_upgradeItemPrefab.GetComponent<RectTransform>().rect.height +
+             parentVerticalLayoutGroup.spacing) *
+            _itemTypes.Count +
+            parentVerticalLayoutGroup.padding.bottom +
+            parentVerticalLayoutGroup.padding.top);
 
         for (int i = 0; i < _itemTypes.Count; i++)
         {
@@ -38,14 +59,13 @@ public sealed class UpgradeMenu : MonoBehaviour
             }
 
             GameObject newUpgrade = Instantiate(_upgradeItemPrefab, _parent);
-            UpgradeItem upgradeItem = newUpgrade.GetComponent<UpgradeItem>();
+            var upgradeItem = newUpgrade.GetComponent<UpgradeItem>();
             upgradeItem.Init(
                 _itemSprites[i],
-                "item",
-                "desc",
+                itemInfo[_itemTypes[i]].name,
+                itemInfo[_itemTypes[i]].description,
                 _upgradeActions[_itemTypes[i]].getLevel,
                 _upgradeActions[_itemTypes[i]].increaseLevel);
         }
-
     }
 }
