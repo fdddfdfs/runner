c0fd6268f1128a963e977d4e4c9098c47cb7a8cb
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-01-24 05:30:44 +0400

    Added exit button

--- Scripts/UI/MainMenu.cs
+++ Scripts/UI/MainMenu.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public sealed class MainMenu : MonoBehaviour
 {
@@ -7,6 +8,7 @@ public sealed class MainMenu : MonoBehaviour
     [SerializeField] private UpgradeMenu _upgradeMenu;
     [SerializeField] private StatsMenu _statsMenu;
     [SerializeField] private Run _run;
+    [SerializeField] private Button _exit;
 
     private bool _isRun;
 
@@ -16,12 +18,15 @@ public sealed class MainMenu : MonoBehaviour
         _startText.gameObject.SetActive(true);
         _statsMenu.ChangeActiveState(true);
         _isRun = false;
+        _exit.gameObject.SetActive(true);
     }
 
     private void Awake()
     {
         _startText.text = "Press F to start run";
         ShowMainMenu();
+        
+        _exit.onClick.AddListener(Application.Quit);
     }
 
     private void Update()
@@ -38,6 +43,7 @@ public sealed class MainMenu : MonoBehaviour
     {
         _upgradeMenu.ChangeMenuVisible(false);
         _statsMenu.ChangeActiveState(false);
+        _exit.gameObject.SetActive(false);
 
         _startText.gameObject.SetActive(false);
         _isRun = true;
