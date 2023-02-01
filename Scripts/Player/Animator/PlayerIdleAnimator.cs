d5a1d734d3a37b195966ec67cff7c01fb67a4de2
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-02 00:09:23 +0400

    Added default implementations for idle animations

--- Scripts/Player/Animator/PlayerIdleAnimator.cs
+++ Scripts/Player/Animator/PlayerIdleAnimator.cs
@@ -1,8 +1,28 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerIdleAnimator : PlayerBaseAnimator
 {
     protected override string AnimatorResourceName => "IdleAnimator";
 
-    public PlayerIdleAnimator(Animator playerAnimator) : base(playerAnimator) { }
+    public PlayerIdleAnimator(Animator playerAnimator) : base(playerAnimator)
+    {
+        _triggerAnimations = new Dictionary<AnimationType, ITriggerAnimation>
+        {
+            { AnimationType.Roll, new EmptyTriggerAnimation() },
+        };
+
+        _boolAnimations = new Dictionary<AnimationType, IBoolAnimation>
+        {
+            { AnimationType.Jump, new EmptyBoolAnimation() },
+            { AnimationType.Land, new EmptyBoolAnimation() },
+            { AnimationType.Fall, new EmptyBoolAnimation() },
+        };
+        
+        _floatAnimations = new Dictionary<AnimationType, IFloatAnimation>
+        {
+            { AnimationType.Speed, new EmptyFloatAnimation() },
+            { AnimationType.HorizontalRun, new EmptyFloatAnimation() }
+        };
+    }
 }
