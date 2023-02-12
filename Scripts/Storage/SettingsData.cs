15fab09683eadba6c4efdc77fecf3a5160c70f55
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-13 01:51:21 +0400

    Added settings menu

---
+++ Scripts/Storage/SettingsData.cs
@@ -0,0 +1,11 @@
+﻿public static class SettingsData
+{
+    private const float StartVolume = 0.5f;
+    private const int DefaultGraphic = 5;
+    
+    public static DataFloat SoundVolume { get; } = new (nameof(SoundVolume), StartVolume);
+
+    public static DataFloat MusicVolume { get; } = new(nameof(MusicVolume), StartVolume);
+
+    public static DataInt Graphic { get; } = new(nameof(Graphic), DefaultGraphic);
+}
---
+++ Scripts/UI/RightMenuMenu/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : Menu
+{
+    [SerializeField] private TMP_Text _musicHeader;
+    [SerializeField] private Slider _musicSlider;
+    [SerializeField] private TMP_Text _musicValue;
+    [SerializeField] private TMP_Text _soundsHeader;
+    [SerializeField] private Slider _soundsSlider;
+    [SerializeField] private TMP_Text _soundsValue;
+    [SerializeField] private TMP_Text _graphicHeader;
+    [SerializeField] private Slider _graphicSlider;
+    [SerializeField] private TMP_Text _graphicValue;
+
+    private readonly string[] _qualities = { "Very Low", "Low", "Medium", "High", "Very High", "Ultra" };
+
+    private void Awake()
+    {
+        _musicHeader.text = "Music";
+        _musicSlider.onValueChanged.AddListener(UpdateMusicVolume);
+        _musicSlider.value = SettingsData.MusicVolume.Value;
+
+        _soundsHeader.text = "Sounds";
+        _soundsSlider.onValueChanged.AddListener(UpdateSoundsVolume);
+        _soundsSlider.value = SettingsData.SoundVolume.Value;
+
+        _graphicHeader.text = "Graphic";
+        _graphicSlider.wholeNumbers = true;
+        _graphicSlider.minValue = 0;
+        _graphicSlider.maxValue = _qualities.Length - 1;
+        _graphicSlider.onValueChanged.AddListener(UpdateGraphic);
+        _graphicSlider.value = SettingsData.Graphic.Value;
+    }
+
+    private void UpdateMusicVolume(float newVolume)
+    {
+        _musicValue.text = ((int)(newVolume * 100)).ToString();
+        SettingsData.MusicVolume.Value = newVolume;
+        Music.Instance.ChangeMusicVolume(newVolume);
+    }
+
+    private void UpdateSoundsVolume(float newVolume)
+    {
+        _soundsValue.text = ((int)(newVolume * 100)).ToString();
+        SettingsData.SoundVolume.Value = newVolume;
+        Sounds.Instance.ChangeSoundsVolume(newVolume);
+    }
+
+    private void UpdateGraphic(float newValue)
+    {
+        var newValueInt = (int)newValue;
+        _graphicValue.text = _qualities[newValueInt];
+        SettingsData.Graphic.Value = newValueInt;
+        QualitySettings.SetQualityLevel(newValueInt);
+    }
+}
