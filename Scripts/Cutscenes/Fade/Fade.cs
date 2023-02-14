3687f2395f1dd4f9dcf706399d12052778e628b0
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-14 06:57:06 +0400

    Added fade animation

---
+++ Scripts/Cutscenes/Fade/Fade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    private const string FadeInTrigger = "FadeIn";
+    private const string FadeOutTrigger = "FadeOut";
+    
+    [SerializeField] private GameObject _fadeObject;
+    [SerializeField] private Animator _fadeAnimator;
+
+    private readonly int _fadeInTrigger = Animator.StringToHash(FadeInTrigger);
+    private readonly int _fadeOutTrigger = Animator.StringToHash(FadeOutTrigger);
+
+    private Action _fadeInCallback;
+    private Action _fadeOutCallback;
+    
+    
+    public void FadeIn(Action callback)
+    {
+        _fadeObject.SetActive(true);
+        _fadeInCallback = callback;
+        _fadeAnimator.SetTrigger(_fadeInTrigger);
+    }
+
+    public void FadeOut(Action callback)
+    {
+        _fadeOutCallback = callback;
+        _fadeAnimator.SetTrigger(_fadeOutTrigger);
+    }
+
+    public void FinishFadeIn()
+    {
+        _fadeInCallback?.Invoke();
+    }
+
+    public void FinishFadeOut()
+    {
+        _fadeOutCallback?.Invoke();
+        _fadeObject.SetActive(false);
+    }
+
+    private void Awake()
+    {
+        _fadeAnimator.GetBehaviour<FadeOutBehaviour>().Fade = this;
+        _fadeAnimator.GetBehaviour<FadeInBehaviour>().Fade = this;
+    }
+}
---
+++ Scripts/Cutscenes/Fade/FadeInBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FadeInBehaviour : StateMachineBehaviour
+{
+    public Fade Fade { get; set; }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        
+        Fade.FinishFadeIn();
+    }
+}
