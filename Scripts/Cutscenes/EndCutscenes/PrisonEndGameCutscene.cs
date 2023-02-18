3266a9c68e3afdaf9573d9bcc61b804253cfeceb
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-18 18:36:20 +0400

    Added end game cutscene

---
+++ Scripts/Cutscenes/EndCutscenes/PrisonEndGameCutscene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class PrisonEndGameCutscene : Cutscene
+{
+    private Action _endCutsceneCallback;
+
+    protected override Action EndCutsceneCallback => _endCutsceneCallback;
+    
+    public void Init(Run run ,Fade fade)
+    {
+        base.Init(fade);
+
+        _endCutsceneCallback = () =>
+        {
+            HideCutscene();
+            fade.FadeOut(null);
+            run.StartRun();
+        };
+    }
+}
