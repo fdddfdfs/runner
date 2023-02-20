ab3d0c5ac8bdee4d9380a8c728cd2134135a5546
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-20 21:00:02 +0400

    Added smooth transaction on end start cutscene

--- Scripts/Cutscenes/StartCutscenes/DeclineLoseStartCutscene/DeclineLoseStartCutscene.cs
+++ Scripts/Cutscenes/StartCutscenes/DeclineLoseStartCutscene/DeclineLoseStartCutscene.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using StarterAssets;
+using UnityEngine;
 
 public class DeclineLoseStartCutscene : Cutscene
 {
+    [SerializeField] private Transform _player;
+    
     private Action _endCutsceneCallback;
     private CutsceneEnvironment _cutsceneEnvironment;
 
     protected override Action EndCutsceneCallback => _endCutsceneCallback;
     
-    public void Init(Run run ,Fade fade, CutsceneEnvironment cutsceneEnvironment)
+    public void Init(Run run ,Fade fade, CutsceneEnvironment cutsceneEnvironment, ThirdPersonController player)
     {
         base.Init(fade);
         _cutsceneEnvironment = cutsceneEnvironment;
@@ -15,9 +19,12 @@ public class DeclineLoseStartCutscene : Cutscene
         _endCutsceneCallback = () =>
         {
             HideCutscene();
-            fade.FadeOut(null);
+            Transform cameraTransform = _cutsceneCamera.transform;
+            player.SetStartRunPosition(
+                _player.transform.position,
+                cameraTransform.position,
+                cameraTransform.rotation);
             run.StartRun();
-            _cutsceneEnvironment.ChangeEnvironmentActive(false);
         };
     }
 
@@ -26,5 +33,20 @@ public class DeclineLoseStartCutscene : Cutscene
         base.SetCutscene();
         
         _cutsceneEnvironment.ChangeEnvironmentActive(true);
+    }
+
+    public override void HideCutscene()
+    {
+        base.HideCutscene();
+        
+        _cutsceneEnvironment.ChangeEnvironmentActive(false);
+    }
+
+    public override void EndCutscene()
+    {
+        if (_isEnded) return;
+        
+        _endCutsceneCallback();
+        TriggerEnd();
     }
 }
