35888e130ebe401223133afbc88925d3344a112b
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-21 18:51:02 +0400

    Added accept lose cutscenes

--- Scripts/Cutscenes/EndCutscenes/AcceptLoseEndCutscene.cs
+++ Scripts/Cutscenes/EndCutscenes/AcceptLoseEndCutscene.cs
@@ -1,4 +1,21 @@
-﻿public class AcceptLoseEndCutscene
+﻿using System;
+
+public class AcceptLoseEndCutscene : CutsceneWithEnvironment
 {
-        
+    private Action _endCutsceneCallback;
+
+    protected override Action EndCutsceneCallback => _endCutsceneCallback;
+    
+    public void Init(Fade fade, CutsceneEnvironment cutsceneEnvironment, MainMenu mainMenu)
+    {
+        base.Init(fade, cutsceneEnvironment);
+
+        _endCutsceneCallback = () =>
+        {
+            HideCutscene();
+            fade.FadeOut(null);
+            mainMenu.SetCutsceneType(typeof(AcceptLoseStartCutscene));
+            mainMenu.ShowMainMenu();
+        };
+    }
 }
--- Scripts/Cutscenes/StartCutscenes/AcceptLoseStartCutscene/AcceptLoseStartCutscene.cs
+++ Scripts/Cutscenes/StartCutscenes/AcceptLoseStartCutscene/AcceptLoseStartCutscene.cs
@@ -1,4 +1,4 @@
-﻿public class AcceptLoseStartCutscene
+﻿public class AcceptLoseStartCutscene : DeclineLoseStartCutscene
 {
     
 }
