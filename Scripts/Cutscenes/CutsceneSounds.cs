c92d68a0c42d5f99879450f37f8d9690f75ac0d3
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-22 22:07:33 +0400

    Added cutscene sounds base class

---
+++ Scripts/Cutscenes/CutsceneSounds.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CutsceneSounds : MonoBehaviour
+{
+    private void PlayLayerOneSound(string soundName)
+    {
+        Sounds.Instance.PlaySound(1, soundName);
+    }
+
+    private void PlayLayerTwoSound(string soundName)
+    {
+        Sounds.Instance.PlaySound(2, soundName);
+    }
+
+    private void PlayLayerThreeSound(string soundName)
+    {
+        Sounds.Instance.PlaySound(3, soundName);
+    }
+
+    private void StopSound(int layer)
+    {
+        Sounds.Instance.StopSound(layer);
+    }
+}
