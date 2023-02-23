2a7eaec8ff8d047c3399bedfbcaa5bdc39f23c55
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-23 21:42:24 +0400

    Fixed bug with layers

--- Scripts/Cutscenes/CutsceneSounds.cs
+++ Scripts/Cutscenes/CutsceneSounds.cs
@@ -4,17 +4,17 @@ public class CutsceneSounds : MonoBehaviour
 {
     private void PlayLayerOneSound(string soundName)
     {
-        Sounds.Instance.PlaySound(1, soundName);
+        Sounds.Instance.PlaySound(0, soundName);
     }
 
     private void PlayLayerTwoSound(string soundName)
     {
-        Sounds.Instance.PlaySound(2, soundName);
+        Sounds.Instance.PlaySound(1, soundName);
     }
 
     private void PlayLayerThreeSound(string soundName)
     {
-        Sounds.Instance.PlaySound(3, soundName);
+        Sounds.Instance.PlaySound(2, soundName);
     }
 
     private void StopSound(int layer)
