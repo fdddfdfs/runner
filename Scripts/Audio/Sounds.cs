f09a2801740da8da9b4ce333bd3a002fd8766e5e
Vsevolod Vasilev <fdddfdfs@yandex.ru>
2023-02-23 21:46:53 +0400

    Added random sound functional

--- Scripts/Audio/Sounds.cs
+++ Scripts/Audio/Sounds.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Sounds : ResourcesSingleton<Sounds, SoundsResourceName>
 {
@@ -8,6 +9,7 @@ public class Sounds : ResourcesSingleton<Sounds, SoundsResourceName>
     [SerializeField] private List<AudioClip> _soundsClips;
 
     private Dictionary<string, AudioClip> _clips;
+    private Dictionary<string, int> _randomClipsCount;
 
     public void ChangeSoundsVolume(float newVolume)
     {
@@ -19,10 +21,7 @@ public class Sounds : ResourcesSingleton<Sounds, SoundsResourceName>
 
     public void PlaySound(int layer, string clipName)
     {
-        if (layer > _soundsSources.Count)
-        {
-            throw new IndexOutOfRangeException($"{layer} more then sounds sources count: {_soundsSources.Count}");
-        }
+        CheckLayer(layer);
 
         clipName = clipName.ToLower();
 
@@ -35,22 +34,84 @@ public class Sounds : ResourcesSingleton<Sounds, SoundsResourceName>
         _soundsSources[layer].Play();
     }
 
+    public void PlayRandomSounds(int layer, string clipBaseName)
+    {
+        CheckLayer(layer);
+        
+        clipBaseName = clipBaseName.ToLower();
+        
+        if (!_randomClipsCount.ContainsKey(clipBaseName))
+        {
+            throw new Exception($"{clipBaseName} doesnt exist int {nameof(Sounds)}");
+        }
+
+        int r = Random.Range(0, _randomClipsCount[clipBaseName]);
+
+        string clipName = clipBaseName + r;
+
+        if (!_clips.ContainsKey(clipName))
+        {
+            throw new Exception(
+                $"Clip with name: {clipName} doesnt exist in {nameof(Sounds)}," +
+                $" clips with {clipBaseName} probably have missing numbers");
+        }
+        
+        _soundsSources[layer].clip = _clips[clipName];
+        _soundsSources[layer].Play();
+    }
+
     public void StopSound(int layer)
     {
-        if (layer > _soundsSources.Count)
-        {
-            throw new IndexOutOfRangeException($"{layer} more then sounds sources count: {_soundsSources.Count}");
-        }
+        CheckLayer(layer);
 
         _soundsSources[layer].Stop();
     }
 
+    public void StopAllSounds()
+    {
+        foreach (AudioSource soundsSource in _soundsSources)
+        {
+            soundsSource.Stop();
+        }
+    }
+
     private void Awake()
     {
+        _randomClipsCount = new Dictionary<string, int>();
         _clips = new Dictionary<string, AudioClip>();
         foreach (AudioClip soundsClip in _soundsClips)
         {
-            _clips.Add(soundsClip.name.ToLower(), soundsClip);
+            string clipName = soundsClip.name.ToLower();
+            
+            if (char.IsDigit(clipName[^1]))
+            {
+                int index = clipName.Length - 1;
+                while (char.IsDigit(clipName[index - 1]))
+                {
+                    index--;
+                }
+
+                string baseName = clipName[index..];
+
+                if (_randomClipsCount.ContainsKey(baseName))
+                {
+                    _randomClipsCount[baseName] += 1;
+                }
+                else
+                {
+                    _randomClipsCount[baseName] = 1;
+                }
+            }
+            
+            _clips.Add(clipName, soundsClip);
+        }
+    }
+
+    private void CheckLayer(int layer)
+    {
+        if (layer > _soundsSources.Count)
+        {
+            throw new IndexOutOfRangeException($"{layer} more then sounds sources count: {_soundsSources.Count}");
         }
     }
 }
