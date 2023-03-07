cfc225fa1979d53fca6d432f4734c4ecd0009b06
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-07 16:30:04 +0300

    Removed unused tag

--- Scripts/UI/ActiveItem/ActiveItem.cs
+++ Scripts/UI/ActiveItem/ActiveItem.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public sealed class ActiveItem: MonoBehaviour
 {
     [SerializeField] private Image _itemImage;
-    [FormerlySerializedAs("_progressImage")] [SerializeField] private List<Image> _progressImages;
+    [SerializeField] private List<Image> _progressImages;
 
     public Image ItemImage => _itemImage;
 
