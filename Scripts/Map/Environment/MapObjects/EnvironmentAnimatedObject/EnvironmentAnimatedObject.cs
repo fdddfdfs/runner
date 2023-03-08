13c4b4b8faf1cce684f04b280699b8843f930661
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-08 14:48:36 +0300

    Added animated objects

---
+++ Scripts/Map/Environment/MapObjects/EnvironmentAnimatedObject/EnvironmentAnimatedObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class EnvironmentAnimatedObject : MonoBehaviour, ITriggerable
+{
+    private const string StartAnimationTriggerName = "Start";
+    
+    [SerializeField] private Animator _objectAnimator;
+
+    private readonly int _startAnimationTrigger = Animator.StringToHash(StartAnimationTriggerName);
+    
+    public void Trigger()
+    {
+        _objectAnimator.SetTrigger(_startAnimationTrigger);
+    }
+}
