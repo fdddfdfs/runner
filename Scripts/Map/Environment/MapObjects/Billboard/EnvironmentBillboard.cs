de88f61eff502e82df4e4963daecbcccfe1d3b59
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-08 14:48:22 +0300

    Added billboards

---
+++ Scripts/Map/Environment/MapObjects/Billboard/EnvironmentBillboard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnvironmentBillboard : MonoBehaviour, ITriggerable
+{
+    [SerializeField] private EnvironmentBillboardInfo _environmentBillboardInfo;
+    [SerializeField] private MeshRenderer _billboard;
+    
+    public void Trigger()
+    {
+        int r = Random.Range(0, _environmentBillboardInfo.SpriteVarients.Count);
+
+        _billboard.material.mainTexture = _environmentBillboardInfo.SpriteVarients[r].texture;
+    }
+}
---
+++ Scripts/Map/Environment/MapObjects/Billboard/EnvironmentBillboardInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnvironmentBillboard", menuName = "Environments/EnvironmentBillboardInfo")]
+public class EnvironmentBillboardInfo : ScriptableObject
+{
+    [SerializeField] private List<Sprite> _spriteVarients;
+
+    public IReadOnlyList<Sprite> SpriteVarients => _spriteVarients;
+}
