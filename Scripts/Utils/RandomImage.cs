ff8b6d14330528a734ceabaa19a8395b89419864
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-16 00:54:00 +0400

    Added image randomizer

---
+++ Scripts/Utils/RandomImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class RandomImage : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private List<Sprite> _sprites;
+
+    private int _previous = -1;
+    
+    private void OnEnable()
+    {
+        int r;
+        do
+        {
+            r = Random.Range(0, _sprites.Count);
+        } while (_sprites.Count > 1 && r == _previous);
+        
+        _image.sprite = _sprites[r];
+        _previous = r;
+    }
+}
