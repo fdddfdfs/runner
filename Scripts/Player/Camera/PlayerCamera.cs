036a519d4d0c3fe5b8c90b22cbfb0357d1b4eba7
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-18 02:35:23 +0400

    Added camera shake

--- Scripts/Player/Camera/PlayerCamera.cs
+++ Scripts/Player/Camera/PlayerCamera.cs
@@ -1,4 +1,6 @@
-﻿using Cinemachine;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerCamera : IRunnable
@@ -6,12 +8,23 @@ public class PlayerCamera : IRunnable
     private readonly CinemachineVirtualCamera _runCamera;
     private readonly CinemachineVirtualCamera _idleCamera;
     private readonly Camera _camera;
+    private readonly ICancellationTokenProvider _cancellationTokenProvider;
+
+    private readonly float _startAmplitude;
+    private readonly CinemachineBasicMultiChannelPerlin _cinemachineBasicMultiChannelPerlin;
     
-    public PlayerCamera(Camera camera, CinemachineVirtualCamera runCamera, CinemachineVirtualCamera idleCamera)
+    public PlayerCamera(
+        Camera camera,
+        CinemachineVirtualCamera runCamera,
+        CinemachineVirtualCamera idleCamera,
+        ICancellationTokenProvider cancellationTokenProvider)
     {
         _runCamera = runCamera;
         _idleCamera = idleCamera;
         _camera = camera;
+        _cancellationTokenProvider = cancellationTokenProvider;
+        _cinemachineBasicMultiChannelPerlin = _runCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _startAmplitude = _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain;
 
         _idleCamera.gameObject.SetActive(false);
         _runCamera.gameObject.SetActive(false);
@@ -28,6 +41,24 @@ public class PlayerCamera : IRunnable
     {
         _runCamera.gameObject.SetActive(false);
         _camera.gameObject.SetActive(false);
+    }
+
+    public async void ShakeCamera(float intensity, float time)
+    {
+        _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        float currentTime = 0;
+        CancellationToken token = _cancellationTokenProvider.GetCancellationToken();
+        while (currentTime < time)
+        {
+            await Task.Yield();
+            currentTime += Time.unscaledDeltaTime * AsyncUtils.TimeScale;
+            _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain =
+                Mathf.Lerp(_startAmplitude, intensity, 1 - currentTime/time);
+
+            if (token.IsCancellationRequested) break;
+        }
+
+        _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = _startAmplitude;
     }
 
     private void SetIdleCamera()
