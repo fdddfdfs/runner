1bb4e6eeb31caa2b929aea7ffad38dc82d03fb71
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-26 18:49:46 +0400

    Added time achievements

---
+++ Scripts/Achievements/TimeAchievement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeAchievement : MonoBehaviour
+{
+    private const int TriggerAchievementDelta = 1;
+    
+    [SerializeField] private List<AchievementData> _timeAchievements;
+
+    private float _currentTime;
+
+    private void FixedUpdate()
+    {
+        _currentTime += Time.fixedDeltaTime * AsyncUtils.TimeScale;
+
+        if (_currentTime < TriggerAchievementDelta) return;
+        
+        foreach (AchievementData timeAchievement in _timeAchievements)
+        {
+            Achievements.Instance.IncreaseProgress(timeAchievement.Name, TriggerAchievementDelta);
+        }
+
+        _currentTime -= TriggerAchievementDelta;
+    }
+}
