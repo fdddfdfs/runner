400f6b4eb65ada05f29651cfa9bf449d79d9a586
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-03-28 21:45:03 +0400

    Removed prefab from abstract data

--- Scripts/Inventory/InventoryItemData.cs
+++ Scripts/Inventory/InventoryItemData.cs
@@ -6,12 +6,10 @@ public abstract class InventoryItemData : ScriptableObject
     [SerializeField] private string _name;
     [SerializeField] private string _description;
     [SerializeField] private Sprite _icon;
-    [SerializeField] private GameObject _prefab;
 
     public int ID => _id;
     public string Name => _name;
     public string Description => _description;
     public abstract InventoryItemType InventoryItemType { get; }
     public Sprite Icon => _icon;
-    public GameObject Prefab => _prefab;
 }
