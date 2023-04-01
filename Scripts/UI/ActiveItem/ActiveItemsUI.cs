fe8a0c511f6074772caca260505248966fb66248
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-01 20:38:42 +0400

    Rewrited with async

--- Scripts/UI/ActiveItem/ActiveItemsUI.cs
+++ Scripts/UI/ActiveItem/ActiveItemsUI.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,44 +12,87 @@ public sealed class ActiveItemsUI : MonoBehaviour, IRunnable
     [SerializeField] private Transform _parent;
     [SerializeField] private GameObject _prefab;
 
+    private readonly CancellationToken[] _linkedTokens = new CancellationToken[1];
+    
     private PoolMono<ActiveItem> _activeItems;
     private Dictionary<ItemType, ActiveItem> _currentlyActiveItems;
     private Dictionary<ItemType, ItemData> _items;
 
+    private CancellationTokenSource _cancellationTokenSource;
+
     public void ShowNewItemEffect(ItemType itemType, float time)
     {
-        ActiveItem item = _currentlyActiveItems.ContainsKey(itemType) 
-            ? _currentlyActiveItems[itemType] 
-            : _activeItems.GetItem();
-        
-        item.ItemImage.sprite = _items[itemType].Icon;
-        foreach (Image progressImage in item.ProgressImages)
-        {
-            progressImage.DOKill();
-            progressImage.fillAmount = 1;
-            progressImage.DOFillAmount(0, time).SetEase(Ease.Linear).OnComplete(() =>
-            {
-                item.gameObject.SetActive(false);
-                _currentlyActiveItems.Remove(itemType);
-            });
-        }
+        ActiveItem item;
 
-        _currentlyActiveItems[itemType] = item;
+        if (_currentlyActiveItems.ContainsKey(itemType))
+        {
+            item = _currentlyActiveItems[itemType];
+            _cancellationTokenSource.Cancel();
+        }
+        else
+        {
+            item = _activeItems.GetItem();
+            item.ItemImage.sprite = _items[itemType].Icon;
+            _currentlyActiveItems[itemType] = item;
+        }
+        
+        CheckCancellationTokenSource();
+        
+        ShowActiveEffect(item, itemType, time);
     }
 
-    public void HideEffect(ItemType itemType)
+    private void CheckCancellationTokenSource()
+    {
+        if (_cancellationTokenSource == null || _cancellationTokenSource.IsCancellationRequested)
+        {
+            _cancellationTokenSource?.Dispose();
+            _linkedTokens[0] = AsyncUtils.Instance.GetCancellationToken();
+            _cancellationTokenSource = CancellationTokenSource.CreateLinkedTokenSource(_linkedTokens);
+        }
+    }
+
+    private async void ShowActiveEffect(ActiveItem item, ItemType itemType, float time)
+    {
+        float currentTime = 0;
+
+        CancellationToken token = _cancellationTokenSource.Token;
+        
+        foreach (Image progressImage in item.ProgressImages)
+        {
+            progressImage.fillAmount = 1;
+        }
+
+        while (currentTime < time)
+        {
+            await Task.Yield();
+            if (AsyncUtils.TimeScale == 0) continue;
+
+            foreach (Image progressImage in item.ProgressImages)
+            {
+                progressImage.fillAmount = 1 - currentTime / time;
+            }
+            
+            currentTime += Time.deltaTime * AsyncUtils.TimeScale;
+            
+            if (token.IsCancellationRequested) return;
+        }
+        
+        HideEffect(itemType, false);
+    }
+
+    public void HideEffect(ItemType itemType, bool isForceCancel = true)
     {
         if (!_currentlyActiveItems.ContainsKey(itemType))
         {
             return;
         }
         
-        ActiveItem item = _currentlyActiveItems[itemType];
-
-        foreach (var progressImage in item.ProgressImages)
+        if (isForceCancel)
         {
-            progressImage.DOKill();
+            _cancellationTokenSource.Cancel();
         }
+        
+        ActiveItem item = _currentlyActiveItems[itemType];
 
         item.gameObject.SetActive(false);
         _currentlyActiveItems.Remove(itemType);
