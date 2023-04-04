c6144f61736e7f7cf7aa0492e076c8eace6dee87
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-04 23:36:35 +0100

    Added clothes setter

---
+++ Scripts/Clothes/ClothesSetter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ClothesSetter : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private List<GameObject> _cloth;
+
+    private void Awake()
+    {
+        Transform[] bones = ClothesManager.FindBones(_player.gameObject, out Transform _);
+        foreach (GameObject cloth in _cloth)
+        {
+            ClothesManager.ApplyClother(_player.gameObject, bones, cloth, ClotherType.Chest);
+        }
+    }
+}
