49d9e065ab4db9c6fb45402fcc3206c778c1bd78
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-09 23:43:26 +0400

    Added fade

--- Scripts/Cutscenes/StartCutscenes/DeclineLoseStartCutscene/DeclineLoseStartCutscene.cs
+++ Scripts/Cutscenes/StartCutscenes/DeclineLoseStartCutscene/DeclineLoseStartCutscene.cs
@@ -4,25 +4,32 @@ using UnityEngine;
 
 public class DeclineLoseStartCutscene : CutsceneWithEnvironment
 {
+    private const float FadeMultiplier = 5f;
+    
     [SerializeField] private Transform _player;
     
     private Action _endCutsceneCallback;
 
     protected override Action EndCutsceneCallback => _endCutsceneCallback;
     
-    public void Init(Run run ,Fade fade, CutsceneEnvironment cutsceneEnvironment, ThirdPersonController player)
+    public void Init(Run run, Fade fade, CutsceneEnvironment cutsceneEnvironment, ThirdPersonController player)
     {
         base.Init(fade, cutsceneEnvironment);
 
         _endCutsceneCallback = () =>
         {
-            HideCutscene();
-            Transform cameraTransform = _cutsceneCamera.transform;
-            player.SetStartRunPosition(
-                _player.transform.position,
-                cameraTransform.position,
-                cameraTransform.rotation);
-            run.StartRun();
+            fade.FadeIn(() =>
+            {
+                HideCutscene();
+                Transform cameraTransform = _cutsceneCamera.transform;
+                player.SetStartRunPosition(
+                    _player.transform.position,
+                    cameraTransform.position,
+                    cameraTransform.rotation);
+                
+                run.StartRun();
+                fade.FadeOut(null, FadeMultiplier);
+            }, FadeMultiplier);
         };
     }
     
--- Scripts/UI/PauseMenu.cs
+++ Scripts/UI/PauseMenu.cs
@@ -3,11 +3,14 @@ using UnityEngine.UI;
 
 public sealed class PauseMenu : MonoBehaviour
 {
+    private const float FadeMultiplier = 5f;
+    
     [SerializeField] private GameObject _menu;
     [SerializeField] private Button _restartButton;
     [SerializeField] private Button _backToMenuButton;
     [SerializeField] private Run _run;
     [SerializeField] private PauseController _pauseController;
+    [SerializeField] private Fade _fade;
 
     public void ChangeMenuActive(bool isActive)
     {
@@ -28,12 +31,17 @@ public sealed class PauseMenu : MonoBehaviour
     {
         _restartButton.onClick.AddListener(() =>
         {
-            _pauseController.ChangePauseState();
+            _fade.FadeIn(() =>
+            {
+                _pauseController.ChangePauseState();
             
-            _run.ApplyLoseResults();
-            _run.EndRun();
-            _run.StartRun();
-            
+                _run.ApplyLoseResults();
+                _run.EndRun();
+                _run.StartRun();
+                
+                _fade.FadeOut(null, FadeMultiplier);
+            }, FadeMultiplier);
+
             Achievements.Instance.GetAchievement("Restart");
         });
         
