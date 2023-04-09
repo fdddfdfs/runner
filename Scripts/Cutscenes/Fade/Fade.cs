cd74fcc4ac4845c4835286424315b4c2808aa4dc
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-09 23:42:30 +0400

    Added fade speed multiplier

--- Scripts/Cutscenes/Fade/Fade.cs
+++ Scripts/Cutscenes/Fade/Fade.cs
@@ -5,27 +5,31 @@ public sealed class Fade : MonoBehaviour
 {
     private const string FadeInTrigger = "FadeIn";
     private const string FadeOutTrigger = "FadeOut";
+    private const string FadeMultiplier = "FadeMultiplier";
     
     [SerializeField] private GameObject _fadeObject;
     [SerializeField] private Animator _fadeAnimator;
 
     private readonly int _fadeInTrigger = Animator.StringToHash(FadeInTrigger);
     private readonly int _fadeOutTrigger = Animator.StringToHash(FadeOutTrigger);
+    private readonly int _fadeMultiplier = Animator.StringToHash(FadeMultiplier);
 
     private Action _fadeInCallback;
     private Action _fadeOutCallback;
     
     
-    public void FadeIn(Action callback)
+    public void FadeIn(Action callback, float fadeMultiplier = 1f)
     {
         _fadeObject.SetActive(true);
         _fadeInCallback = callback;
+        _fadeAnimator.SetFloat(_fadeMultiplier, fadeMultiplier);
         _fadeAnimator.SetTrigger(_fadeInTrigger);
     }
 
-    public void FadeOut(Action callback)
+    public void FadeOut(Action callback, float fadeMultiplier = 1f)
     {
         _fadeOutCallback = callback;
+        _fadeAnimator.SetFloat(_fadeMultiplier, fadeMultiplier);
         _fadeAnimator.SetTrigger(_fadeOutTrigger);
     }
 
