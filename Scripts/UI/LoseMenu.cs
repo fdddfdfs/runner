52b8a73fe661cfc77c00646488b94222ef96d202
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-10 22:01:57 +0400

    Added fade

--- Scripts/UI/LoseMenu.cs
+++ Scripts/UI/LoseMenu.cs
@@ -4,13 +4,15 @@ using UnityEngine.UI;
 
 public sealed class LoseMenu : MonoBehaviour
 {
+    private const float FadeMultiplier = 5f;
+    
     [SerializeField] private GameObject _menu;
     [SerializeField] private TMP_Text _score;
     [SerializeField] private TMP_Text _money;
     [SerializeField] private Button _restart;
     [SerializeField] private Button _backToMenu;
     [SerializeField] private Run _run;
-
+    [SerializeField] private Fade _fade;
 
     public void ShowLoseMenu(int score, int money)
     {
@@ -31,9 +33,13 @@ public sealed class LoseMenu : MonoBehaviour
     {
         _restart.onClick.AddListener(() =>
         {
-            ChangeMenuVisible(false);
-            _run.EndRun();
-            _run.StartRun();
+            _fade.FadeIn(() =>
+            {
+                ChangeMenuVisible(false);
+                _run.EndRun();
+                _run.StartRun();
+                _fade.FadeOut(null, FadeMultiplier);
+            }, FadeMultiplier);
         });
         
         _backToMenu.onClick.AddListener(() =>
