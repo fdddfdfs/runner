b22697022598f169f9c727fd7dfb92b3706c656a
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-11 20:54:49 +0400

    Added environment sound trigger

---
+++ Scripts/Map/Environment/MapObjects/EnvironmentSound/EnvironmentSound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnvironmentSound : MonoBehaviour, ITriggerable
+{
+    private const int EnvironmentSoundLayer = 3;
+
+    [SerializeField] private string _soundName;
+    [SerializeField] private bool _isRandomSound;
+
+    public void Trigger()
+    {
+        if (_isRandomSound)
+        {
+            Sounds.Instance.PlayRandomSounds(EnvironmentSoundLayer, _soundName);
+        }
+        else
+        {
+            Sounds.Instance.PlaySound(EnvironmentSoundLayer, _soundName);
+        }
+    }
+}
