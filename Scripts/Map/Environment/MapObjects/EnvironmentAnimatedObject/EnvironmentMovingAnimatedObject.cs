b744667bf106cf79dc9e8eddb1919eae755c7b61
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-13 20:31:28 +0400

    Added environment moving animated objects

---
+++ Scripts/Map/Environment/MapObjects/EnvironmentAnimatedObject/EnvironmentMovingAnimatedObject.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EnvironmentMovingAnimatedObject : EnvironmentAnimatedObject
+{
+    [SerializeField] private Transform _object;
+    
+    private Vector3 _startPosition;
+    private Quaternion _startRotation;
+    
+    private void Start()
+    {
+        if (_object == null)
+        {
+            _object = transform;
+        }
+        
+        Transform objectTransform = _object.transform;
+        _startPosition = objectTransform.position;
+        _startRotation = objectTransform.rotation;
+    }
+
+    private void OnDisable()
+    {
+        Transform objectTransform = _object.transform;
+        objectTransform.position = _startPosition;
+        objectTransform.rotation = _startRotation;
+    }
+}
