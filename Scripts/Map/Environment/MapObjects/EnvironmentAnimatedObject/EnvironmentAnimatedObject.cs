afc6d2a51ef721850e3549641082fa993ac3ff7a
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-13 20:32:06 +0400

    Added auto initialisation

--- Scripts/Map/Environment/MapObjects/EnvironmentAnimatedObject/EnvironmentAnimatedObject.cs
+++ Scripts/Map/Environment/MapObjects/EnvironmentAnimatedObject/EnvironmentAnimatedObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class EnvironmentAnimatedObject : MonoBehaviour, ITriggerable
 {
@@ -7,7 +8,20 @@ public class EnvironmentAnimatedObject : MonoBehaviour, ITriggerable
     [SerializeField] private Animator _objectAnimator;
 
     private readonly int _startAnimationTrigger = Animator.StringToHash(StartAnimationTriggerName);
-    
+
+    private void Awake()
+    {
+        if (_objectAnimator == null)
+        {
+            _objectAnimator = GetComponent<Animator>();
+
+            if (_objectAnimator == null)
+            {
+                throw new Exception("Cannot find animator");
+            }
+        }
+    }
+
     public void Trigger()
     {
         _objectAnimator.SetTrigger(_startAnimationTrigger);
