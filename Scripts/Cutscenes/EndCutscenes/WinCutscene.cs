4cdeba65e27a9a443eff92e944aad0bbde309830
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-21 22:50:37 +0400

    Added win cutscene

---
+++ Scripts/Cutscenes/EndCutscenes/WinCutscene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StarterAssets;
+using UnityEngine;
+
+public class WinCutscene : Cutscene
+{
+    private Action _endCutsceneCallback;
+
+    protected override Action EndCutsceneCallback => _endCutsceneCallback;
+    
+    public void Init(ThirdPersonController player, Run run, Fade fade)
+    {
+        base.Init(fade);
+
+        transform.parent = player.transform;
+        transform.localPosition = Vector3.zero;
+        
+        _endCutsceneCallback = () =>
+        {
+            HideCutscene();
+            fade.FadeOut(null);
+            run.SetMainMenuCutscene(typeof(WinStartCutscene));
+            run.BackToMenu(false);
+        };
+    }
+}
---
+++ Scripts/Cutscenes/StartCutscenes/WinCutscene/WinStartCutscene.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class WinStartCutscene : Cutscene
+{
+    private const float FadeMultiplier = 5f;
+    
+    private Action _endCutsceneCallback;
+
+    protected override Action EndCutsceneCallback => _endCutsceneCallback;
+    
+    public void Init(Run run, Fade fade)
+    {
+        base.Init(fade);
+
+        _endCutsceneCallback = () =>
+        {
+            fade.FadeIn(() =>
+            {
+                HideCutscene();
+                run.StartRun();
+                fade.FadeOut(null, FadeMultiplier);
+            }, FadeMultiplier);
+        };
+    }
+    
+    public override void EndCutscene()
+    {
+        if (_isEnded) return;
+        
+        _endCutsceneCallback();
+        TriggerEnd();
+    }
+}
