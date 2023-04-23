b445739f464122247d5faca63c528a987922b201
Vasilev Vsevolod <fdddfdfs@yandex.ru>
2023-04-23 22:18:42 +0400

    Added environment moving object

---
+++ Scripts/Map/Environment/MapObjects/EnvironmentAnimatedObject/EnvironmentMovingObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class EnvironmentMovingObject : MonoBehaviour, ITriggerable
+{
+    [SerializeField] private Transform _object;
+    [SerializeField] private Vector3 _direction;
+
+    private Vector3 _startPosition;
+    private bool _isActive;
+    
+    private void FixedUpdate()
+    {
+        if (!_isActive) return;
+        
+        _object.localPosition += _direction * AsyncUtils.TimeScale;
+    }
+
+    private void Awake()
+    {
+        if (_object == null)
+        {
+            _object = transform;
+        }
+        
+        _startPosition = _object.localPosition;
+    }
+
+    private void OnEnable()
+    {
+        _object.localPosition = _startPosition;
+    }
+
+    public void Trigger()
+    {
+        _isActive = true;
+    }
+}
